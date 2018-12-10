e7e0c539c73ba9141d9fd9772a62c8e08d46f5e8
manuqudos <manu.qudos@gmail.com>
2018-12-10 20:17:00 +0300

    add js changes

---
+++ script.js
@@ -0,0 +1,23 @@
+function run(){
+
+ var question1=parseInt('input[name="question1"]:checked').value;
+ var question2=parseInt('input[name="question2"]:checked').value;
+ var question3=parseInt('input[name="question3"]:checked').value;
+ var question4=parseInt('input[name="question4"]:checked').value;
+ var question4=parseInt('input[name="question5"]:checked').value;
+ var correct=0;
+
+  if (if question1 === "Pellentesque cursus justo") {
+   correct +10}
+
+  if (if question2 === "Pellentesque cursus justo") {
+   correct +10}
+
+  if (if question3 === "Pellentesque cursus justo") {
+   correct +10}
+
+  if (if question4 === "Pellentesque cursus justo") {
+   correct +10}
+
+  if (if question5 === "Pellentesque cursus justo") {
+   correct +10}
